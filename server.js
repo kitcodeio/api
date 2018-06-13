9237c7fb7eaadde75abd8a3b9b45d4ce75f6f9aa
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-06-13 15:19:09 +0530

    config, user and crypto file added

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const config = require('./config.json');
+const api = require('./lib/api');
+
 const app = express();
-const PORT = 8080;
+const env = process.env.NODE_ENV || 'development';
 
 app.use(cors());
 app.use(bodyParser.json());
 
 async function start() {
-  try{
-    await app.listen(PORT, ()=>{
-      console.log("Server is online at port:"+PORT);
+  try {
+    await app.listen(config[env].port, () => {
+      console.log("Server is online at port:" + config[env].port);
     });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
