0dd1b52e6107e3ac14072200c8b8b38bb0be6013
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-07-14 18:51:10 +0530

    added socket connection support

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('express-jwt');
+const socket = require('socket.io');
 
 const config = require('./config/serverconfig.json');
 const routes = require('./lib/routes');
 const errorHandler = require('./lib/errorHandler');
+const sockets = require('./lib/socket.js');
 
 const app = express();
 const env = process.env.NODE_ENV || 'development';
+const server  = require('http').createServer(app);
+
 
 app.use(cors());
 app.use(express.static('public'));
@@ -22,9 +26,13 @@ app.use(routes);
 
 app.use(errorHandler);
 
+
+var io = require('socket.io')(server);
+io.on('connection', sockets);
+
 async function start() {
   try {
-    await app.listen(config[env].port, () => {
+    await server.listen(config[env].port, () => {
       console.log(`Server is online at port:${config[env].port}`);
     });
   } catch (err) {
