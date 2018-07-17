5ad4e7114f5ff0d64b8d9acceb1db4e3c51fd09d
satnam-sandhu <singhsandhusatnam@outlook.com>
2018-07-17 15:43:54 +0530

    added rbac and course read apis exposed

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ const jwt = require('express-jwt');
 const socket = require('socket.io');
 
 const config = require('./config/serverconfig.json');
+const rbac = require('./lib/rbac');
 const routes = require('./lib/routes');
 const errorHandler = require('./lib/errorHandler');
 const sockets = require('./lib/socket.js');
 
 const app = express();
 const env = process.env.NODE_ENV || 'development';
-const server  = require('http').createServer(app);
+const server = require('http').createServer(app);
 
+const unless = [/\/read\/course\/.*/, '/login', '/register'];
 
 app.use(cors());
 app.use(express.static('public'));
 app.use(jwt({
   secret: config[env].jwtsecret
 }).unless({
-  path: ['/login','/register']
+  path: unless
 }));
+app.use(rbac());
 app.use(bodyParser.json());
 app.use(routes);
 
