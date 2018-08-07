e53328537c3c7c2bb92905827581a1ef12eb24a8
Satnam Singh <singhsandhusatnam@outlook.com>
2018-08-07 16:08:32 +0530

    added migration for container location

---
+++ migrations/20180807094947-add-location-column.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return [
+      queryInterface.addColumn('Containers', 'ip_location', Sequelize.STRING),
+      queryInterface.addColumn('Containers', 'geo_location', Sequelize.STRING)
+    ];
+  },
+  down: (queryInterface, Sequelize) => {
+    return [
+      queryInterface.removeColumn('Containers', 'ip_location'),
+      queryInterface.removeColumn('Containers', 'geo_location')
+    ];
+  }
+};
