c1612f6668d5e565dd28220526df258303b2ac37
Satnam Singh <singhsandhusatnam@outlook.com>
2018-08-13 17:57:05 +0530

    added hapijwt and configured inert

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-exports.register = function(plugin, options, next) {
+const Inert = require('inert');
+const JWT = require('hapi-auth-jwt2');
 
+const Routes = require('./lib/route');
+
+async function validate(decoded, request, cb) {
+  return cb(null, true)
+}
+
+exports.register = async function(plugin, options, next) {
+  const config = options.config;
+  await plugin.register([Inert, JWT]);
+  plugin.auth.strategy('jwt', 'jwt', {
+    key: config.jwtsecret,
+    validateFunc: validate,
+    verifyOptions: {
+      algorithms: ['HS256']
+    }
+  });
+  plugin.route(Routes);
   return next();
 }
 
