5b51fdf4ac9547581b3290f85b44e3cb52aaf35f
Satnam Singh <singhsandhusatnam@outlook.com>
2018-08-16 22:06:54 +0530

    updated config object behaviour

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const JWT = require('hapi-auth-jwt2');
 const Routes = require('./lib/routes');
 const Validate = require('./lib/validate');
 const Sockets = require('./lib/socket');
-var config = require("./config/config.json");
+const env = process.env.NODE_ENV || 'staging';
+
+var apiConfig = require("./config/config.json")[env];
 
 exports.register = async function(plugin, options, next) {
-  config = options.config || config;
+  config = options.config || apiConfig;
+  config.db = apiConfig.db;
   const io = require('socket.io')(plugin.listener);
 
   plugin.register([Inert, JWT], function(err) {
