5ba9e37f14d401258312f817bcffa8c9e002b66d
Satnam Singh <singhsandhusatnam@outlook.com>
2018-08-17 15:06:56 +0530

    added strategy register function

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 const Inert = require('inert');
 const JWT = require('hapi-auth-jwt2');
+const Bell = require('bell');
+
 const Routes = require('./lib/routes');
-const Validate = require('./lib/validate');
 const Sockets = require('./lib/socket');
+const strategy = require('./lib/strategy');
+
 const env = process.env.NODE_ENV || 'staging';
 
-exports.register = function(plugin, options, next) {
-  config = options.config;
-  const io = require('socket.io')(plugin.listener);
-  plugin.register([Inert, JWT], function(err) {
-    if (err) throw err;
-    plugin.auth.strategy('jwt', 'jwt', {
-      key: config.jwtsecret,
-      validateFunc: Validate,
-      verifyOptions: {
-        algorithms: ['HS256']
-      }
-    });
-    plugin.route(Routes(config));
-    io.on('connection', Sockets);
-  });
+
+exports.register = async function(plugin, options, next) {
+  const config = options.config;
+  const io = require('socket.io')(plugin.listener); 
+  io.on('connection', Sockets);
+  await plugin.register([Inert, Bell, JWT]);
+  strategy.register(plugin, config);
+  plugin.route(Routes(config));
   return next();
 }
 
