510eb27b1d49fe56999c73a42e8975e8e5882760
Satnam Singh <singhsandhusatnam@outlook.com>
2018-12-29 13:44:05 +0530

    run api added for dangling containers

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Inert = require('inert');
 const JWT = require('hapi-auth-jwt2');
-const Bell = require('bell');
 const Routes = require('./lib/routes');
 const Sockets = require('./lib/socket');
 const strategy = require('./lib/strategy');
@@ -10,7 +9,7 @@ exports.register = async function(plugin, options, next) {
   const config = options.config;
   const io = require('socket.io')(plugin.listener); 
   io.on('connection', Sockets(config));
-  await plugin.register([Inert, Bell, JWT]);
+  await plugin.register([Inert, JWT]);
   strategy.register(plugin, config);
   plugin.route(Routes(config, io));
   return next();
