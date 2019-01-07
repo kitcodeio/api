1e87cb663db70b62b8c5c9232b673f538994db6c
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-07 08:37:45 +0530

    added tutorial addition and approval api

---
+++ migrations/20190106231845-create-tutorial.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tutorials', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      link: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          is: {
+            args: /((([A-Za-z]{4,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+            msg: 'that doesn\'t look like a valid link'
+          }
+        }
+      },
+      tags: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      approved: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tutorials');
+  }
+};
---
+++ models/tutorial.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tutorial = sequelize.define('Tutorial', {
+    id: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: {
+          args: /((([A-Za-z]{4,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+          msg: 'that doesn\'t look like a valid link'
+        }
+      }
+    },
+    tags: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    approved: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {});
+  Tutorial.associate = function(models) {};
+  return Tutorial;
+};
