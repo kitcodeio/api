47298952ce28ce2390306167653f2d409fccf085
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-21 12:54:19 +0530

    remove dockerfile gen and restructured models

--- models/Client.js
+++ models/Client.js
@@ -3,26 +3,47 @@ const jwt = require('../utils/jwt');
 
 var Client = (function() {
 
-  function Client(config, schema) {
-    
-    this.name = 'Client';
-    
-    this.authenticate = async function(email, password) {
-      let client = await schema.Client.findOne({
-        where: {
-          email: email,
-        },
-      });
-      if (!client) return;
-      client = client.toJSON();
-      if (crypto.hash(password, client.salt, config.secret.algorithm) !== client.password_hash) return;
-      if (!client.verified) return;
+  var config;
+  var schema;
 
-      return {
-        token: jwt.generate(client, config.secret.jwt)
-      };
+  function Client(_config, _schema) {
+    this.name = 'Client';
+    config = _config;
+    schema = _schema;
+  };
+
+  Client.prototype.authenticate = async function(email, password) {
+    let client = await schema.Client.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!client) return;
+    client = client.toJSON();
+    if (crypto.hash(password, client.salt, config.secret.algorithm) !== client.password_hash) return;
+    if (!client.verified) return;
+
+    return {
+      token: jwt.generate(client, config.secret.jwt)
+    };
+  };
+
+  Client.prototype.create = async function(data) {
+    let client;
+
+    data.salt = crypto.generateSalt();
+    data.password_hash = crypto.hash(data.password, data.salt, config.secret.algorithm);
+    delete data.password;
+    data.role_type = 'user';
+
+    try {
+      client = await schema.Client.create(data);
+      return client;
+    } catch (err) {
+
+      return;
     }
-  
   }
 
   return Client;
