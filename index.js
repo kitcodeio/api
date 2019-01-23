007a7ab488cc858021bb5d4bb32a9c5b8493cd6d
Satnam Singh <singhsandhusatnam@outlook.com>
2019-01-23 13:30:22 +0530

    socket.js restrutured, container api cleaned

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Inert = require('inert');
 const JWT = require('hapi-auth-jwt2');
 const Routes = require('./lib/routes');
-const Sockets = require('./lib/socket');
+const Socket = require('./lib/Socket');
 const strategy = require('./lib/strategy');
 
 exports.register = async function(plugin, options, next) {
   const config = options.config;
-  const io = require('socket.io')(plugin.listener); 
-  io.on('connection', Sockets(config));
+  const io = new Socket(config, plugin.listener).io;
+
   await plugin.register([Inert, JWT,]);
   strategy.register(plugin, config);
   plugin.route(Routes(config, io));
+
   return next();
 };
 
