ac4645796a6cd874e35310ffe8462d3b5c28691a
Satnam Singh <singhsandhusatnam@outlook.com>
2019-02-01 18:58:00 +0530

    added sessions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Inert = require('inert');
 const JWT = require('hapi-auth-jwt2');
+const Cookie = require('hapi-auth-cookie');
 const Routes = require('./lib/routes');
 const Socket = require('./lib/Socket');
 const strategy = require('./lib/strategy');
@@ -8,7 +9,7 @@ exports.register = async function(plugin, options, next) {
   const config = options.config;
   const io = new Socket(config, plugin.listener).io;
 
-  await plugin.register([Inert, JWT,]);
+  await plugin.register([Inert, JWT, Cookie]);
   strategy.register(plugin, config);
   plugin.route(Routes(config, io));
 
