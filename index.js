8154c894882d283490b071142112f899c174b51b
souparno <souparno.majumder@gmail.com>
2019-02-12 00:01:27 +0530

    api singleton architecture should be created with routes

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const Inert = require('inert');
 const JWT = require('hapi-auth-jwt2');
 const Cookie = require('hapi-auth-cookie');
 const Routes = require('./lib/routes');
-const Socket = require('./lib/Socket');
 const strategy = require('./lib/strategy');
 
 exports.register = async function(plugin, options, next) {
   const config = options.config;
-  const io = new Socket(config, plugin.listener).io;
 
   await plugin.register([Inert, JWT, Cookie]);
   strategy.register(plugin, config);
